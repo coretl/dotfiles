01c69afde59913cb9f3323a8487bfa889ddbdb00
Tom Cobb <tom.cobb@diamond.ac.uk>
2021-02-11 17:00:38 +0000

    fix history and install

--- bashrc.sh
+++ bashrc.sh
@@ -18,7 +18,7 @@ shopt -s histappend
 # Ensure Gnome Terminal shows the terminal title correctly
 PROMPT_COMMAND='echo -ne "\033]2;${USER}@${HOSTNAME}: ${PWD/$HOME/~}\007"'
 # Append each command to history immediately after it ran, dont' wait until the session ends
-PROMPT_COMMAND="history -a; history -c; history -r;$PROMPT_COMMAND"
+PROMPT_COMMAND="history -a;$PROMPT_COMMAND"
 # Eliminate adjacent history lines
 HISTCONTROL=ignoreboth
 
